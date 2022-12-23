35a9bf2cb2bf886a153a6a4719634c50dc4a19a2
Oleg <proverkail@mail.ru>
2022-12-23 12:46:34 +0300

    Создал печать массива

--- Rabota/Program.cs
+++ Rabota/Program.cs
@@ -1,7 +1,18 @@
 ﻿void Zadanie3()
 {
     string[] numbers = new string[] { "Moskva", "Peter", "I", "It", "Car", "Up" };
+    FillArrau(numbers);
 
 }
-
+void FillArrau(string[] nums)
+{
+    Console.WriteLine("Дан массив слов:");
+    int size = nums.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{nums[i]}, ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
 Zadanie3();
